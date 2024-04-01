39393ff01cc8b5633229cbdf5b8cb60431cbb7eb
Anubhav Gain <iamanubhavgain@gmail.com>
2024-04-01 23:49:32 +0530

    todo: had to fix the changes

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,25 @@
-use std::net::TcpStream;
 use std::io::{Read, Write};
+use std::net::TcpStream;
+use rustls::{ClientConfig, ClientSession, Stream};
+
+const SERVER_ADDR: &str = "127.0.0.1:8080";
 
 fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:8080").expect("Failed to connect to server");
-    println!("Connected to server!");
+    let config = load_tls_config();
+    let stream = TcpStream::connect(SERVER_ADDR).expect("Failed to connect to server");
+    let hostname = webpki::DNSNameRef::try_from_ascii_str(SERVER_ADDR.split(':').next().unwrap()).unwrap();
+    let stream = rustls::ClientSession::new(&config, hostname);
+    let mut stream = Stream::new(stream, stream);
 
-    let message = "Hello, server!";
-    stream.write_all(message.as_bytes()).expect("Failed to write to server");
+    stream.write_all(b"Hello, server!").expect("Failed to write to server");
 
     let mut response = String::new();
     stream.read_to_string(&mut response).expect("Failed to read response from server");
     println!("Server responded: {}", response);
 }
 
+fn load_tls_config() -> ClientConfig {
+    let mut config = ClientConfig::new();
+    config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+    config
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,12 @@
-use std::net::{TcpListener, TcpStream};
+use std::fs;
 use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use rustls::{NoClientAuth, ServerConfig, ServerSession, Stream};
 
-fn handle_client(mut stream: TcpStream) {
+const CERT_FILE: &str = "cert.pem";
+const KEY_FILE: &str = "key.pem";
+
+fn handle_client(mut stream: Stream<TcpStream, ServerSession>) {
     let mut buf = [0; 1024];
     while match stream.read(&mut buf) {
         Ok(size) => {
@@ -22,6 +27,7 @@ fn handle_client(mut stream: TcpStream) {
 }
 
 fn main() {
+    let config = load_tls_config();
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind");
     println!("Server listening on port 8080");
 
@@ -29,8 +35,10 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
+                let config = config.clone();
                 std::thread::spawn(move || {
-                    handle_client(stream);
+                    let _ = config;
+                    let _ = handle_client(stream);
                 });
             }
             Err(e) => {
@@ -40,3 +48,14 @@ fn main() {
     }
 }
 
+fn load_tls_config() -> ServerConfig {
+    let cert_file = fs::read(CERT_FILE).expect("Failed to read certificate file");
+    let key_file = fs::read(KEY_FILE).expect("Failed to read key file");
+
+    let mut config = ServerConfig::new(NoClientAuth::new());
+    let cert = rustls::internal::pemfile::certs(&mut &cert_file[..]).unwrap();
+    let key = rustls::internal::pemfile::pkcs8_private_keys(&mut &key_file[..]).unwrap().remove(0);
+    config.set_single_cert(cert, key).unwrap();
+
+    config
+}
